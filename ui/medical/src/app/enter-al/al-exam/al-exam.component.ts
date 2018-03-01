4dc937aa9c576125d24571ce0fbaddfcfe3f8263
OhHelloWorld <18255629607@163.com>
2018-03-01 13:18:59 +0800

    update al-tongue

--- ui/medical/src/app/enter-al/al-exam/al-exam.component.ts
+++ ui/medical/src/app/enter-al/al-exam/al-exam.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-al-exam',
@@ -10,6 +11,7 @@ export class AlExamComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
